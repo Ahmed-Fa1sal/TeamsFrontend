28c4632b4de375439bfe2c7e1777c5b0c410fa10
Ahmed <DerRiese765@gmail.com>
2026-06-08 11:53:33 +0300

    loading spinner added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { APP_ROUTES } from './app.routes';
 import { jwtInterceptor } from '@core/interceptors/jwt.interceptor';
+import { loadingInterceptor } from '@core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(APP_ROUTES),
     provideAnimations(),
-    provideHttpClient(withInterceptors([jwtInterceptor])),
+    provideHttpClient(withInterceptors([loadingInterceptor, jwtInterceptor])),
   ],
 };
